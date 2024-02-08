2d3199eac15e8a804f0836054e7a34ec9253d6a2
Salehuddin Loke <salehuddinloke.asriloke@etiqa.com.my>
2024-02-08 13:31:12 +0800

    ADDED: Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  const config = new DocumentBuilder()
+    .setTitle('PDF Sample Generator')
+    .setDescription('Building Sample data for AI Trainings')
+    .setVersion('1.0')
+    .addTag('PDF')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  console.log('Swagger hosted at:', 'localhost:3000/api')
   await app.listen(3000);
 }
 bootstrap();
